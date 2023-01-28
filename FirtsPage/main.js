9d5503878510cbfb880310501d4430213075c89f
christian <chrislacruz2005@gmail.com>
2023-01-28 13:19:06 -0400

    estamos empezando a ver como se integra Js al HTNL !

---
+++ FirtsPage/main.js
@@ -0,0 +1,16 @@
+console.log("Hola mundo");
+
+const h1 = document.querySelector("h1");
+const p = document.querySelector('p');
+const parra = document.querySelector('.pas');
+const pib = document.querySelector('#pib');
+const input = document.querySelector('input');
+
+
+console.log({
+    h1, 
+    p, 
+    parra, 
+    pib, 
+    input,
+})
--- POO/FabricaAutos.js
+++ POO/FabricaAutos.js
@@ -14,7 +14,7 @@ for(let i = 0 ; i < 30 ; i++){
     var marca = prompt("Ingresa la marca del auto");
     var modelo = prompt("Ingresa el modelo del auto");
     var annio = prompt("Ingresa el año del auto");
-    autos.push(new auto (marca, modelo, annio));
+    autos.push(new autos (marca, modelo, annio));
   }
   
   for(let i = 0 ; i < autos.length ; i++){
